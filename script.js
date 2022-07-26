bbd140bb98c79f91ea203f20d7beddfd6d1fd94a
Hassan Abdulreda <108088689+v7sn@users.noreply.github.com>
2022-07-26 15:14:00 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const years = document.getElementById('years');
+const months = document.getElementById('months');
+const days = document.getElementById('days');
+const hours = document.getElementById('hours');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+const milliseconds = document.getElementById('milliseconds');
+
+
+function displayTime() {
+	let time = new Date();
+	years.textContent = time.getFullYear();
+	months.textContent = time.getMonth();
+	days.textContent = time.getDay();
+	hours.textContent = time.getHours();
+	minutes.textContent = time.getMinutes();
+	seconds.textContent = time.getSeconds();
+	milliseconds.textContent = time.getMilliseconds();
+
+	if (time.getHours() > 12) {
+		hours.textContent = time.getHours() - 12;
+	}
+}
+
+displayTime();
+
+setInterval(displayTime, 1);
